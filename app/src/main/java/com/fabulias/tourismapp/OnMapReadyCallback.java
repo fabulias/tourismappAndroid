c4b4b110bb797b04d45baeba07aceb3ac4c36cc2
yxam <mbeltraoyarzun@gmail.com>
2016-11-11 23:28:50 -0300

    Index, Evauation, Perfil user

---
+++ app/src/main/java/com/fabulias/tourismapp/OnMapReadyCallback.java
@@ -0,0 +1,11 @@
+package com.fabulias.tourismapp;
+
+import com.google.android.gms.maps.GoogleMap;
+
+/**
+ * Created by maxymiliano on 10-11-16.
+ */
+public interface OnMapReadyCallback {
+    void onMapReady(GoogleMap googleMap);
+}
+
---
+++ app/src/main/java/com/fabulias/tourismapp/evaluation.java
@@ -0,0 +1,13 @@
+package com.fabulias.tourismapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class evaluation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluation);
+    }
+}
